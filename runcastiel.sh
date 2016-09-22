e6426c9056f29eae338f7456a7cf2e7c5ed7baf9
kohldampfer <kristian.starkloff@web.de>
2016-09-21 20:43:15 -0400

    symbolic link to data directory

--- runcastiel.sh
+++ runcastiel.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+
+TARGET_DATA_DIR=$(dirname $0)/target/data
+# delete old symbolic link if exists
+if [[ -L "$TARGET_DATA_DIR" &&  -d "$TARGET_DATA_DIR" ]]; then
+	rm -v "TARGET_DATA_DIR"
+fi
+
+# create new symbolic link
+ln -s $(dirname $0)/data $TARGET_DATA_DIR
+
+# start castiel
 java -jar $(dirname $0)/target/castiel-1.0.jar $*
