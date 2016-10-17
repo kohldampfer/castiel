9ad9d27e6687fc7e16018d7fa73e67239b4ea371
kohldampfer <kristian.starkloff@web.de>
2016-10-17 22:50:50 +0200

    Creating resource dir for tests.

--- src/test/java/TestPhpLexer1.java
+++ src/test/java/TestPhpLexer1.java
@@ -1,5 +1,5 @@
-import junit.framework.*;
-import org.junit.*;
+import junit.framework.Assert;
+import org.junit.Test;
 import java.io.*;
 import org.homeunix.wap.php.parser.*;
 import org.antlr.runtime.*;
@@ -7,14 +7,23 @@ import org.antlr.runtime.*;
 public class TestPhpLexer1 {
 	
 	@Test
-	public void testReadFileAndGetLexerStream() {
+	public void testReadFileAndGetLexerStream() throws IOException {
 		// read file
 		String fileName = "filter.php";
-		InputStream is = new FileInputStream(fileName);
-		
-		// create input stream from standard input
-		ANTLRInputStream input = new ANTLRInputStream(is);
-		// Create a lexer attached to that input stream
-		PhpLexer lexer = new PhpLexer(input);
+
+		try {
+			InputStream is = new FileInputStream(fileName);
+
+			// create input stream from standard input
+			ANTLRInputStream input = new ANTLRInputStream(is);
+			// Create a lexer attached to that input stream
+			PhpLexer lexer = new PhpLexer(input);
+		} catch (FileNotFoundException f) {
+			File file = new File(fileName);
+			String fullPath = file.getAbsolutePath();
+
+			System.out.println("Could not find test file at " + fullPath);
+
+		}
 	}
 }
